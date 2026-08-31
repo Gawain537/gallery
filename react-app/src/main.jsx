34b64b1b0bea45ba21d595094a04739bce4c96e9
Sean <seanrobertoconnor@yahoo.com>
2026-08-31 17:50:40 -0400

    Updated website text

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
 
 createRoot(document.getElementById('root')).render(
-  <h1>Welcome!</h1>
+  <h1>Welcome, this site is under construction! Source: https://github.com/Gawain537/gallery</h1>
 )  
